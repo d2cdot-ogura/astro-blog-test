d1f5a9a5f6bc4b6aa1424ec303d99ef64cf9b361
小倉 真帆路 <mahoro.ogura@d2cdot.co.jp>
2025-02-12 16:16:27 +0900

    SSRの設定追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: 'server', // SSRにする
+    adapter: vercel(), // Vercelに最適な形でデプロイするための設定 
+});
