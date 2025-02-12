fa547ec2504edb81815fe304ee2629b090bbce53
小倉 真帆路 <mahoro.ogura@d2cdot.co.jp>
2025-02-12 16:31:55 +0900

    ISRを実装

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
     output: 'server', // SSRにする
-    adapter: vercel(), // Vercelに最適な形でデプロイするための設定 
+    adapter: vercel({
+        isr: {
+            expiration: 60, // 60秒間キャッシュする
+        }
+    }), // Vercelに最適な形でデプロイするための設定 
 });
